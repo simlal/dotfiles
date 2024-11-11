c2d807ab56dd559146c6fec77dcb0e00af068aee
Simon Lalonde <simlalonde@hotmail.com>
2024-11-11 09:29:59 -0500

    Enable/disable conform

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -211,6 +211,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- [[ Disable format-on-save in conform ]]
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -228,6 +228,10 @@ return {
 			-- log_level = vim.log.levels.DEBUG, -- Enable debugging logs
 			notify_on_error = true,
 			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
 				-- Disable "format_on_save lsp_fallback" for languages that don'tconf
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.

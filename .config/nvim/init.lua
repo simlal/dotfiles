83430becd1209b94e2d3e92a3e9e8fa345118a89
Simon Lalonde <simlalonde@hotmail.com>
2024-11-11 08:20:05 -0500

    Remaps for resize

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,6 +168,12 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlight fr
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+-- Faster window resize
+vim.api.nvim_set_keymap("n", "<C-w>>", "<cmd> vertical resize +15<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-w><", "<cmd> vertical resize -15<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-w>+", "<cmd> horizontal resize +15<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-w>-", "<cmd> horizontal resize -15<CR>", { noremap = true, silent = true })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
